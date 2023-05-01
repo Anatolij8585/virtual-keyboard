8166cba2cec6151f3ccad47ecfc4e396db00cd0c
natolij8585 <anatolij8585@bk.ru>
2023-05-01 21:07:12 +0300

    feat: installed a webpack and eslint

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const esLintPlugin = (isDev) => isDev ? [] : [ new ESLintPlugin({ extensions: ['ts', 'js'] }) ];
+
+const devServer = (isDev) => !isDev ? {} : {
+  devServer: {
+    open: true,
+    hot: true,
+    port: 8080,
+    static: path.join(__dirname, 'development'),
+  },
+};
+
+module.exports = ({ development }) => ({
+  mode: development ? 'development' : 'production',
+  devtool: development ? 'inline-source-map' : false,
+  entry: {
+    main: './src/index.js',
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/[hash][ext]',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+    new HtmlWebpackPlugin({ title: 'Virtual keyboard' }),
+    new CopyPlugin({
+      patterns: [
+        // { from: 'public' },
+        {
+            from: path.resolve(__dirname, "./src/assets"),
+            to: path.resolve(__dirname, "./dist/assets"),
+            noErrorOnMissing: true
+        },
+      ],
+    }),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    ...esLintPlugin(development),
+  ],
+  resolve: {
+    extensions: ['.js'],
+  },
+  ...devServer(development)
+});
